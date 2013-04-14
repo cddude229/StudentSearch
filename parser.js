3e931f58842c2dfe4bb91d80343a90fe0d754d6c
Sarah <sarahscodel5450@gmail.com>
2013-04-14 13:56:25 -0400

    adding parser

--- parser.js
+++ parser.js
@@ -6,10 +6,68 @@ ALLOWED ASSUMPTIONS
 3) The words "and" and "or" can be in any capitalization, but will be nicely separated from
    potential values
 */
+var andValues = ["and", "And", "AND", "+", "/", ","];
+var orValues = ["or", "Or", "OR", "+", "/", ","];
 
-var parser = function(currentString){
-    var listOfPossibles = [];
+var parser = function(currentString) {
+  var listOfPossibilities = [];
+  var terms = termsList(currentString.split(/[\s,+]+/));
+  console.log("string: " + currentString + " ,terms: " + JSON.stringify(terms));
+  if (terms.length == 1) {
+    console.log('returning 0:' + terms[0]);
+    listOfPossibilities.push(terms[0]);
+    return listOfPossibilities;
+  } else {
+    // check if the second to last term is either an and or or, if so recurse
+    var start = currentString.indexOf(terms[terms.length - 3]);
+    var end = start + terms[terms.length - 3].length;
+    var subOptions = parser(currentString.substring(0, end));
+    console.log("else suboptions: " + JSON.stringify(subOptions));
+    if (andValues.indexOf(terms[terms.length - 2]) > -1) {
+      for (var i = 0; i < subOptions.length; i++) {
+        if (subOptions[i].split(" ").length > 2) {
+          listOfPossibilities.push("(" + subOptions[i] + ")" + " AND " + terms[terms.length - 1]);
+        } else {
 
-    // The return needs to be a list of possible values, in good format
-    return listOfPossibles;
+          listOfPossibilities.push(subOptions[i] + " AND " + terms[terms.length - 1]);
+        }
+
+      }
+    }
+    if (orValues.indexOf(terms[terms.length - 2]) > -1) {
+      for (var g = 0; g < subOptions.length; g++) {
+        if (subOptions[g].split(" ").length > 2) {
+          listOfPossibilities.push("(" + subOptions[g] + ")" + " OR " + terms[terms.length - 1]);
+        } else {
+
+          listOfPossibilities.push(subOptions[g] + " OR " + terms[terms.length - 1]);
+        }
+      }
+    }
+  }
+  removeParenthesis(listOfPossibilities);
+  // The return needs to be a list of possible values, in good format
+  return listOfPossibilities;
 };
+
+var termsList = function(termsArray) {
+  for (var x = 1; x < termsArray.length; x++) {
+    if (andValues.indexOf(termsArray[x]) === -1 && andValues.indexOf(termsArray[x - 1]) === -1 && orValues.indexOf(termsArray[x - 1]) === -1 && orValues.indexOf(termsArray[x]) === -1) {
+      // have to add a commea between the two terms
+      termsArray.splice(x, 0, ",");
+      return termsList(termsArray);
+    }
+  }
+  return termsArray;
+};
+
+var removeParenthesis = function(arr) {
+  for (var x = 0; x < arr.length; x++) {
+    term = arr[x];
+    if (!(term.indexOf("OR") > -1 && term.indexOf("AND") > -1)) {
+      // we only have one type of term so we don't need any parenthesis
+      arr[x] = term.replace(/[()]/g, '')
+    }
+  }
+
+}
--- tests/tests_parser.js
+++ tests/tests_parser.js
@@ -8,6 +8,7 @@ test("convert commas", function(){
     };
 
     testResult("a, b", ["a AND b", "a OR b"], "Basic comma test");
+    testResult("a, b, c", ["a AND b AND c", "a OR b OR c", "(a AND b) OR c", "(a OR b) AND c"], "complicated comma test");
     testResult("a, b, or c", ["(a AND b) OR c", "a OR b OR c"], "Or-comma test");
     testResult("a, b, and c", ["a AND b AND c"], "And-comma test");
 });

79cb231fa4357470e5aa842be2194e4cc0638a0a
Chris Dessonville <chris@24-1-128.dynamic.csail.mit.edu>
2013-04-16 04:03:24 -0400

    Plug in the autocomplete

--- logic.js
+++ logic.js
@@ -1,3 +1,11 @@
 $(function(){
     updateResults(students, 1);
+
+    // Ok, setup the filters for parser.js
+    $("#courses, #skills").autocomplete({
+        source: function(request, response){
+            response(parser(request.term));
+        }
+    });
+
 });

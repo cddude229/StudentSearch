041807ba79518ed1e63edcce993fdd62ad5f696b
Chris Dessonville <chris@24-1-128.dynamic.csail.mit.edu>
2013-04-16 04:03:17 -0400

    Move images.  Add jquery-ui's missing CSS file + images

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-var imagesDir = "./images/"; // Path of images for students
+var imagesDir = "./images/students/"; // Path of images for students
 var baseYear = "2013"; // What class are students?
 var interestsCardLimit = 5; // Max number of interests to show on a student's card
 var studentsPerPage = 8; // Students per page

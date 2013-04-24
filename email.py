ccd337746a86a1b6c56293ccabc5c2038d1e6aa7
Sarah <sarahscodel5450@gmail.com>
2013-04-24 18:21:12 -0400

    adding the ability to write emailed students to a file

---
+++ email.py
@@ -0,0 +1,16 @@
+import shelve
+
+def addStudents(professor, listOfStudents):
+  d = shelve.open("emails")
+  if d.has_key(professor):
+    d[professor] = d[professor]+filter(lambda x:x not in d[professor],listOfStudents)
+  else:
+    d[professor] = listOfStudents
+  d.close()
+
+def getStudents(professor):
+  d = shelve.open("emails")
+  if d.has_key(professor):
+    return d[professor]
+  else:
+    return []

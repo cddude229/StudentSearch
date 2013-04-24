893599c0440bf2fe5c42a168d919d1a77ac8651a
Chris Dessonville <chris@dke-one-o-two.mit.edu>
2013-04-24 00:27:03 -0400

    Get images working in static files.

--- static/constants.js
+++ static/constants.js
@@ -1,4 +1,4 @@
-var imagesDir = "./images/students/"; // Path of images for students
+var imagesDir = "./static/images/students/"; // Path of images for students
 var baseYear = "2013"; // What class are students?
 var interestsCardLimit = 3; // Max number of interests to show on a student's card
 var studentsPerPage = 8; // Students per page

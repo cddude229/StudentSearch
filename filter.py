dc605a09b0feec4830e2d516bc3d4ce69720d0c2
Sarah <sarahscodel5450@gmail.com>
2013-04-29 14:35:20 -0400

    adding terminator, making it work with filter

--- filter.py
+++ filter.py
@@ -1,23 +1,32 @@
  # @param objects        List of JS objects that should be filtered
  # @param attr           The attribute list of the object to check against
  # @param grouping       As built by the Terminator.
+import terminator
 
-def objectFilter(objects, attr, grouping):
-  return objects # TEMPORARY UNTIL GROUPING TAKES STRINGS
+print "inside filter"
 
+def objectFilter(objects, attr, string):
+  return filterObjects(objects, attr, terminator.terminator(string))
 
+def filterObjects(objects, attr, grouping):
+  print "New grouping: " , grouping, " type: " , type(grouping)
   filteredObjects = [];
-  if type(grouping) == 'string':
+  if type(grouping) != type({}):
+    print "grouping is string"
     return [obj for obj in objects if grouping.lower() in obj[attr]]
   elif len(grouping["items"])==0:
+    print "length is 0"
     return objects
   if grouping["type"] == "AND":
+    print "type is AND"
     newObjects = list(objects);
     for i in range(len(grouping["items"])):
-      intersect = [val for val in newObjects if val in objectFilter(objects, attr, grouping["items"][i])]
+      intersect = [val for val in newObjects if val in filterObjects(objects, attr, grouping["items"][i])]
       newObjects = list(intersect)
     return newObjects;
   elif grouping["type"] == "OR":
+    print "type is OR"
     for x in range(len(grouping["items"])):
-      filteredObjects= list(set(filteredObjects) | set(objectFilter(objects, attr, grouping["items"][x])))
+      filteredObjects.extend(filterObjects(objects, attr, grouping["items"][x]))
     return filteredObjects
+  return filteredObjects
---
+++ terminator.py
@@ -0,0 +1,58 @@
+import string
+
+# When given a good search string, this will return a list of groupings to use.
+def terminator(str):
+    # State
+    currentItems = []
+    unparsedTerms = []# Used for groupings only
+    groupDepth = 0
+    currentTerm = ""
+    lastType = "AND"#Default to AND.  Only case where this is used if it's only one item
+
+    for a in range(len(str)):
+        ch = str[a]
+        if ch == "(":
+            groupDepth+=1
+            currentTerm += ch
+        elif ch == ")":
+            groupDepth-=1
+            currentTerm += ch
+            # Ok, recursively pass down
+            if groupDepth == 0:
+                unparsedTerms.append(currentTerm);
+                combined = string.join(unparsedTerms," ")
+                index0 = string.find(combined,"(")
+                index1 = string.rfind(combined,")")
+                combined = combined[:index1] + combined[index1+1:]
+                combined = combined[:index0] + combined[index0+1:]
+                toAppend = terminator(combined)
+                if toAppend!="":
+                    currentItems.append(toAppend)
+                unparsedTerms = [] # Reset unparsed terms
+                currentTerm = "" # Clear term
+                a+=1 # Skip the next space
+            
+        elif ch == " ":
+            if groupDepth == 0:
+                #Ok, not in parenthesis and hit a gap.
+                # See if it was an AND or OR, and record it
+                if (currentTerm == "AND" or currentTerm == "OR"):
+                    lastType = currentTerm
+                else:
+                    if currentTerm!="":
+                        currentItems.append(currentTerm)
+            else:
+                if currentTerm!="":
+                    unparsedTerms.append(currentTerm)
+            #ok, finally add last term
+            currentTerm = ""
+        else:
+            currentTerm += ch
+        
+    # Add last item, if there
+    if len(currentTerm) > 0:
+        currentItems.append(currentTerm)
+    # return our result
+    return {"type" : lastType, "items" : currentItems}
+    
+print terminator("(6.813 AND 6.860) OR python")
